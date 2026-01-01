6ef905d9a1a5464dbe2a23968622a4e6dce96093
Shayan Taherkhani <shayanthn78@gmail.com>
2026-01-01 11:36:40 -0600

    Particle to Image

---
+++ ep6-Particle to Image/mySketch.js
@@ -0,0 +1,86 @@
+//Coded by Shayan Taherkhani shayantaherkhani.ir
+///IG : Coding1dea
+OPC.slider('curliness', 1, 0, 3);
+OPC.slider('velocity', 1, 0.1, 10);
+OPC.toggle('screenshot', false);
+
+let img;
+let particles = [];
+let edgeColour;
+
+function preload(){
+	img = loadImage("shayan.jpg");
+}
+
+function setup() {
+	createCanvas(1112, 834);
+	img.resize(width, 0);
+	background(255);
+
+	
+	colorMode(HSB, 255);
+	
+	edgeColour = color(random(255), 255, 255, 10);
+	
+	for(let i = 0; i < 600; i++){
+		particles[i] = new Particle(edgeColour, curliness);
+	}
+}
+
+
+
+function draw() {
+	if (screenshot == true){
+    save("img_" + month() + '-' + day() + '_' + hour() + '-' + minute() + '-' + second() + ".jpg");
+		screenshot = false;
+	}
+
+	for(let i = 0; i < particles.length; i++){
+      
+      
+    if(particles[i].pos.y < 0 || particles[i].pos.y > height 
+			                        || particles[i].pos.x < 0 
+			                        || particles[i].pos.x > width)
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
